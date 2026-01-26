7f0214082ad7fe38dde3961df4e5721d345db98b
tunecraftbrasil-cmyk <tunecraftbrasil@gmail.com>
2026-01-26 12:15:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,9 @@ const SUPABASE_URL = "https://miupzfchvfbqbznfhvix.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pdXB6ZmNodmZicWJ6bmZodml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxOTYwNzksImV4cCI6MjA4NDc3MjA3OX0.rz0W9qVovRvAeyBQ55LRewOAOM5a8pNJs1-UwWttATw";
 const STRIPE_PUBLIC_KEY = "pk_test_51StZvo1wgBnYhkbFGEWUqMCIhCzXBCzUrDxypLj7ge0InE6ZlQj4QXWHPmljSyaLqaC4hNxZvA0oZ4PWCEh6FJj10072HL9w8v";
 
+// Mercado Pago (coloque sua Public Key aqui)
+const MERCADOPAGO_PUBLIC_KEY = "APP_USR-ba19e69a-29d4-4346-824e-5c99ed4047b3";
+
 // Evita redeclaração se já estiver carregado
 if (!window.TUNECRAFT_CONFIG_LOADED) {
   window.TUNECRAFT_CONFIG_LOADED = true;
